68836f6500c2863fc8e6ddf301306e61ca59a18b
Alex Romaniuk <alaster.rom@gmail.com>
2020-06-17 17:01:33 +0300

    add file

---
+++ src/DifNum.java
@@ -0,0 +1,19 @@
+public class DifNumbers {
+
+   public static void main(String[] args) {
+      int num1 = 5;
+	  int num2 = 15;
+	  int dif;
+      dif = num1 - num2;
+      logger.log("Sum of these numbers: " + dif);
+   }
+   
+   public static void main(String[] args) {
+      int red1 = 5;
+	  int red2 = 17;
+	  int rif;
+      rif = red1 - red2;
+      System.out.println("Sum of these numbers: " + rif);
+   }
+   
+}
