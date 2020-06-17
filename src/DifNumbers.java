1620450d2b281fcbb5c00daf3f552e46072309ac
Alex Romaniuk <alaster.rom@gmail.com>
2020-06-17 18:39:38 +0300

    add

---
+++ src/DifNumbers.java
@@ -0,0 +1,10 @@
+public class DifNumbers {
+
+   public static void main(String[] args) {
+      int sun1 = 5;
+	  int sun2 = 15;
+	  int def;
+      def = sun1 - sun2;
+      logger.log("Sum of these numbers: " + def);
+   }
+}
