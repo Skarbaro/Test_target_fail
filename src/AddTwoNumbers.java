e52b5bc5e0f7c6fc97bee5be9ce10b4782d72e23
Alex Romaniuk <alaster.rom@gmail.com>
2020-06-18 08:30:03 +0300

    adfs

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -5,7 +5,8 @@ public class AddTwoNumbers {
    public static void main(String[] args) {
       
       int num1 = 5;
-	  int num2 = 15, sum;
+	  int num2 = 15;
+	  int sum;
 
       sum = num1 + num2;
       
