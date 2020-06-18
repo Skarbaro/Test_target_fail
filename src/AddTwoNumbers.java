6b50450234941ff780aeb9954b3e3bc8e935c0e1
Alex Romaniuk <alaster.rom@gmail.com>
2020-06-18 10:12:43 +0300

    add

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -4,8 +4,7 @@ public class AddTwoNumbers {
    
    public static void main(String[] args) {
       
-      int num1 = 5;
-	  int num2 = 15;
+      int num1 = 5, num2 = 15;
 	  int sum;
 
       sum = num1 + num2;
