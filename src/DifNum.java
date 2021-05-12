1d2b95cb15daa4fc1e0eb577f84b890af707d1a4
Alex Romaniuk <alaster.rom@gmail.com>
2021-05-12 14:15:14 +0300

    update file 1 commit

--- src/DifNum.java
+++ src/DifNum.java
@@ -1,8 +1,7 @@
 public class DifNumbers {
 
    public static void main(String[] args) {
-      int num1 = 5;
-	  int num2 = 15;
+      int num1 = 5, int num2 = 19;
 	  int dif;
       dif = num1 - num2;
       logger.log("Sum of these numbers: " + dif);
@@ -10,7 +9,7 @@ public class DifNumbers {
    
    public static void main(String[] args) {
       int red1 = 5;
-	  int red2 = 17;
+	  int red2 = 21;
 	  int rif;
       rif = red1 - red2;
       System.out.println("Sum of these numbers: " + rif);
