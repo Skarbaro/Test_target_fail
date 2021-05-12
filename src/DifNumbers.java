6a6e68a4c7a8fc8a811ff75a3166b8d774b5ab2d
Alex Romaniuk <alaster.rom@gmail.com>
2021-05-12 15:13:54 +0300

    addte

--- src/DifNumbers.java
+++ src/DifNumbers.java
@@ -3,8 +3,7 @@ package src;
 public class DifNumbers {
 
    public static void main(String[] args) {
-      int sun1 = 5;
-	  int sun2 = 15;
+      int sun1 = 5, sun2 = 17;
 	  int def;
       def = sun1 - sun2;
       logger.log("Sum of these numbers: " + def);
