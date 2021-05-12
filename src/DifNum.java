1c9e7ca5d9dc982d5b4f1e40711085a3683c16de
Alex Romaniuk <alaster.rom@gmail.com>
2021-05-12 15:17:33 +0300

    ters

--- src/DifNum.java
+++ src/DifNum.java
@@ -2,6 +2,7 @@ public class DifNumbers {
 
    public static void main(String[] args) {
       int num1 = 5, int num2 = 19;
+	  int ter = 3;
 	  int dif;
       dif = num1 - num2;
       logger.log("Sum of these numbers: " + dif);
