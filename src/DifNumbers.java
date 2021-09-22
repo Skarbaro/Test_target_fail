72c89bc619eb2749e383c8fbe3132229f3482cc8
Alex 111 <alaster.111@gmail.com>
2021-09-22 15:17:34 +0300

    update 1 file DifNumbers.java #12

--- src/DifNumbers.java
+++ src/DifNumbers.java
@@ -2,8 +2,9 @@ package src;
 
 public class DifNumbers {
 
-   public static void main(String[] args) {
-      int sun1 = 5, sun2 = 17, def;
+   public static void main(String args[]) {
+      int sun1 = 5; 
+	  int sun2 = 17, def;
       def = sun1 - sun2;
       logger.log("Sum of these numbers: " + def);
 	  System.out.println("Sum of these numbers: " + def);
