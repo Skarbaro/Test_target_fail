d0f8e77b805271cb94e3c51eaa25b4d40639743b
Alex Romaniuk <alaster.rom@gmail.com>
2021-10-14 11:19:31 +0300

    updatee file DifNumber for test e11000

--- src/DifNumbers.java
+++ src/DifNumbers.java
@@ -1,11 +1,12 @@
-package srcv.java;
+package src;
 
 public class DifNumbers {
 
    public static void main(String args[]) {
       int sun1 = 5; 
+	  int sum3 = 7;
 	  int sun2 = 19, def;
-      def = sun1 - sun2;
+      def = sun1 - sun2 + sum3;
       logger.log("Sum of these numbers: " + def);
 	  System.out.println("Sum of these numbers: " + def);
    }
