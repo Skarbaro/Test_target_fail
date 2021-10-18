90dfb31225c856bd9ea1905ff60e0e9285c49654
Alex Romaniuk <alaster.rom@gmail.com>
2021-10-18 10:53:59 +0300

    refactor files in repository #23

--- src/java/DifNumbers.java
+++ src/java/DifNumbers.java
@@ -3,8 +3,7 @@ package src;
 public class DifNumbers {
 
    public static void main(String args[]) {
-      int sun1 = 5; 
-	  int sum3 = 7;
+      int sun1 = 5, sum3 = 90;
 	  int sun2 = 19, def;
       def = sun1 - sun2 + sum3;
       logger.log("Sum of these numbers: " + def);
