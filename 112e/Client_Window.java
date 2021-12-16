9c38f554e08acb039e29d87e21abe9db4bdc0f40
Alex Romaniuk <alaster.rom@gmail.com>
2021-12-16 16:50:07 +0200

    add ed file 2

---
+++ 112e/Client_Window.java
@@ -0,0 +1,29 @@
+public class AddTwoNumbers {
+	
+	private String cat;
+	private String color;
+	
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 15, sum;
+		sum = num1 + num2;
+		System.out.println("Sum of these numbers: " + sum);
+	}
+	
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 15, del;
+		del = num1 + num2;
+		System.out.println("Del of these numbers: " + del);
+	}
+	
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 15, del;
+		del = num1 + num2;
+		System.out.println("Del of these numbers: " + del);
+	}
+	
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 15, del;
+		del = num1 + num2;
+		System.out.println("Del of these numbers: " + del);
+	}
+}
